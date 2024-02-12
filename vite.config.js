451b1d8bb5e14c4d3f273cf82a9e853484e971b1
AbdunabiA <abdunabiabduvahobov86@gmail.com>
2024-02-12 12:49:02 +0500

    compression

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import jsconfigPaths from "vite-jsconfig-paths";
-import { compression } from "vite-plugin-compression2";
+// import { compression } from "vite-plugin-compression2";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     jsconfigPaths(),
-    compression({
-      // algorithm: "brotliCompress",
-      // exclude: [/\.(br)$/, /\.(gz)$/],
-      deleteOriginalAssets: true,
-    }),
     ViteImageOptimizer(),
   ],
 });
