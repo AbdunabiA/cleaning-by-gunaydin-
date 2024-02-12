7096292437ca9ba55a4295b066370c3496cc6d8c
AbdunabiA <abdunabiabduvahobov86@gmail.com>
2024-02-12 15:25:26 +0500

    image

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import jsconfigPaths from "vite-jsconfig-paths";
-import viteImagemin from "@vheemstra/vite-plugin-imagemin";
 // import { compression } from "vite-plugin-compression2";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
-import imageminWebp from "imagemin-webp";
-import imageminMozjpeg from "imagemin-mozjpeg";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     jsconfigPaths(),
-    // ViteImageOptimizer(),
-    viteImagemin({
-      plugins: {
-        jpg: imageminMozjpeg(),
-      },
-      makeWebp: {
-        plugins: {
-          jpg: imageminWebp(),
-        },
-      },
-    }),
+    ViteImageOptimizer(),
+    // viteImagemin({
+    //   plugins: {
+    //     jpg: imageminMozjpeg(),
+    //   },
+    //   makeWebp: {
+    //     plugins: {
+    //       jpg: imageminWebp(),
+    //     },
+    //   },
+    // }),
   ],
 });
